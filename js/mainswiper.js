4f335f1dad025b88f60bb300839c90373c3c0728
John Doe <lachilov23@icloud.com>
2025-03-09 02:41:30 +0500

    Update mainswiper.js

--- js/mainswiper.js
+++ js/mainswiper.js
@@ -1,11 +1,11 @@
 const mainSwiper = document.querySelector(".container__main__home");
 const mainPhotos = [
   "./img/home.webp",
-  "../img/projects/001/4_1.webp",
-  "../img/projects/003/11.webp",
-  "../img/projects/003/12.webp",
-  "../img/projects/006/1.webp",
-  "../img/projects/006/7.webp",
+  "./img/projects/001/4_1.webp",
+  "./img/projects/003/11.webp",
+  "./img/projects/003/12.webp",
+  "./img/projects/006/1.webp",
+  "./img/projects/006/7.webp",
 ];
 let i = 0;
 
