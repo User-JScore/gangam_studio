58c907ecd543df83df6bb2cbbceeebddfec11f72
Lachilov V. <lachilov23@gmail.com>
2025-03-14 17:30:13 +0500

    focus+target

---
+++ js/anim.js
@@ -0,0 +1,43 @@
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_anim-active");
+      } else {
+        if (!animItem.classList.contains("_anim-no-hide"))
+          animItem.classList.remove("_anim-active");
+      }
+    }
+  }
+
+  function offset(element) {
+    const rect = element.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {
+      top: rect.top + scrollTop,
+      left: rect.left + scrollLeft,
+    };
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
